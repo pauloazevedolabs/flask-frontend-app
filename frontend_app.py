15f103b7c376cab1612d8cc79d77109154d63aee
pauloazevedolabs <pauloazevedovw114@gmail.com>
2025-11-19 21:58:40 +0000

    Update frontend_app.py

--- frontend_app.py
+++ frontend_app.py
@@ -1,43 +1,20 @@
-import os
-from flask import Flask, render_template, request, redirect, url_for
-import requests
+resource "azurerm_linux_web_app" "webapp" {
+  name                = "paulolabfrontend"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  service_plan_id     = azurerm_service_plan.lab_appservice.id
 
+  site_config {
+    python_version     = "3.10"
+    app_command_line   = "gunicorn -w 2 -b 0.0.0.0:8000 app:app"
+    health_check_path  = "/health"
+  }
 
-BACKEND_BASE = "http://134.149.58.75:5000"  # VM private IP
+  app_settings = {
+    "WEBSITES_PORT" = "8000"
+  }  
 
-# ---------- Insert Page ----------
-@app.route("/", methods=["GET", "POST"])
-def insert_page():
-    message = None
-    if request.method == "POST":
-        first = request.form.get("first")
-        last = request.form.get("last")
-        dept = request.form.get("dept")
-        try:
-            r = requests.post(
-                f"{BACKEND_BASE}/api/insert",
-                json={"FirstName": first, "LastName": last, "Department": dept},
-                timeout=5
-            )
-            r.raise_for_status()
-            message = "✅ Row inserted successfully!"
-            return redirect(url_for("table_page"))
-        except Exception as e:
-            message = f"❌ Insert failed: {e}"
-    return render_template("insert.html", message=message)
-
-# ---------- Table Page ----------
-@app.route("/table")
-def table_page():
-    try:
-        r = requests.get(f"{BACKEND_BASE}/api/data", timeout=5)
-        r.raise_for_status()
-        employees = r.json()
-    except Exception as e:
-        employees = []
-
-    return render_template("table.html", employees=employees)
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
+  depends_on = [
+    azurerm_service_plan.lab_appservice
+  ]
+}
