431ce74394ead475ed788a297342a718498adc8b
pauloazevedolabs <pauloazevedovw114@gmail.com>
2025-11-20 10:32:39 +0000

    Update frontend_app.py

--- frontend_app.py
+++ frontend_app.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!", 200
+    
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
